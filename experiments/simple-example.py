48e0d5b0add6ae51af07c4807753226b599488f2
maximiliano <maximiliano.rojas.1@ug.uchile.cl>
2024-12-11 16:16:37 -0300

    simple example added

---
+++ experiments/simple-example.py
@@ -0,0 +1,34 @@
+import numpy as np
+import sys
+sys.path.append('../src')
+
+# Import the package
+from TSP import TSP
+
+# Initialize NumPy arrays of shape (n_samples, dim) with random 
+# variables samples in Fortran-contiguous memory order
+X = np.array([-0.2, 0.5, 0.0, 0.0, 0.4, 2.0, -0.4, 1.0, 0.2, 3.0], order='F')
+Y = np.array([1.2, -0.5, 0.0, 0.0, 0.7, -1.0, 2.4, -1.2, 1.2, 8.0], order='F')
+
+# Initialize the TSP with its parameters
+l_bn = 0.167
+w_bn = 5e-2
+reg_factor = 2.5e-5
+tsp = TSP(l_bn, w_bn, reg_factor)
+
+# Set observations to the estimator
+tsp.grow(X, Y)
+
+# Get TSP size and estimated mutual information
+size = tsp.size()
+emi = tsp.emi()
+
+# Regularize the TSP mutual information estimator
+tsp.regularize()
+
+# Get the regularized TSP size and estimated mutual information
+reg_size = tsp.reg_size()
+reg_emi = tsp.reg_emi()
+
+# Visualize the space partitions in a 2D scenario
+tsp.visualize(X, Y)
